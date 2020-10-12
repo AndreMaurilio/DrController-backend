9ac9ee942430f9cdebdffdb134a44fc503730ac4
Andre Maurilio Java <andrehakon@gmail.com>
2020-10-12 16:41:02 -0300

    modificações para versao da maquina virtual

--- src/main/resources/db/changelog/scripts/script_login.sql
+++ src/main/resources/db/changelog/scripts/script_login.sql
@@ -6,15 +6,15 @@
 CREATE TABLE
 IF NOT EXISTS `tg_bancodedados`.`aut_autorizacao`
 (
-  `AUT_ID` BIGINT
+  `aut_id` BIGINT
 (20)  NOT NULL AUTO_INCREMENT,
-  `AUT_NOME` VARCHAR
+  `aut_nome` VARCHAR
 (35) NOT NULL,
   PRIMARY KEY
-(`AUT_ID`),
+(`aut_id`),
   --UNIQUE INDEX `UNI_AUT_NOME` (`AUT_NOME` ASC))ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET=utf8;
-    UNIQUE INDEX `UNI_AUT_NOME`
-(`AUT_NOME` ASC))ENGINE = InnoDB DEFAULT CHARSET=utf8;
+    UNIQUE INDEX UNI_AUT_NOME
+(`aut_nome` ASC))ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
 --ROLLBACK drop table AUT_AUTORIZACAO;
 
@@ -26,12 +26,12 @@ IF NOT EXISTS `tg_bancodedados`.`uau_usuario_autorizacao`
 
   `usu_id` BIGINT
 (20)  NOT NULL,
-  `AUT_ID` BIGINT
+  `aut_id` BIGINT
 (20)  NOT NULL,
   PRIMARY KEY
 (`usu_id`),
-  INDEX `AUT_AUTORIZACAO_FK`
-(`AUT_ID` ASC),
+  INDEX `aut_autorizacao_fk`
+(`aut_id` ASC),
   CONSTRAINT `uau_usuario_autorizacao_ibfk_1`
     FOREIGN KEY
 (`usu_id`)
@@ -41,9 +41,9 @@ IF NOT EXISTS `tg_bancodedados`.`uau_usuario_autorizacao`
 UPDATE CASCADE,
   CONSTRAINT `uau_usuario_autorizacao_ibfk_2`
     FOREIGN KEY
-(`AUT_ID`)
+(`aut_id`)
     REFERENCES `tg_bancodedados`.`aut_autorizacao`
-(`AUT_ID`)
+(`aut_id`)
     ON
 UPDATE CASCADE)ENGINE = InnoDB
 DEFAULT CHARSET=utf8;
@@ -61,7 +61,7 @@ VALUES
 --rollback delete from tg_bancodedados.usuario where usu_id=3566;
 
 --changeset andre:09
-INSERT INTO tg_bancodedados.usuario
+INSERT INTO `tg_bancodedados`.`usuario`
   (usu_idcad,usu_disciplina,usu_email,usu_nome,usu_funcao,usu_senha)
 VALUES
   ('MacPro-RN', 'TUBULAÇÃO', 'ronald@petro.com', 'Ronaldo', 'Supervisor', CONCAT
@@ -71,7 +71,7 @@ VALUES
 --rollback delete from tg_bancodedados.usuario where usu_id=8977;
 
 --changeset andre:10
-INSERT INTO tg_bancodedados.usuario
+INSERT INTO `tg_bancodedados`.`usuario`
   (usu_idcad,usu_disciplina,usu_email,usu_nome,usu_funcao,usu_senha)
 VALUES
   ('ASUS-ICQ', 'TI', 'muh@petro.com', 'Murilo', 'Administrador', CONCAT
@@ -83,50 +83,50 @@ VALUES
 
 
 --changeset andre:11
-INSERT INTO tg_bancodedados.AUT_AUTORIZACAO
-  (AUT_NOME)
+INSERT INTO `tg_bancodedados`.`aut_autorizacao`
+  (`aut_nome`)
 VALUES
   ('ROLE_DESEN');
 
---rollback delete from tg_bancodedados.AUT_AUTORIZACAO where AUT_NOME='ROLE_DESEN';
+--rollback delete from tg_bancodedados.aut_autorizacao where AUT_NOME='ROLE_DESEN';
 
 --changeset andre:12
-INSERT INTO tg_bancodedados.AUT_AUTORIZACAO
-  (AUT_NOME)
+INSERT INTO `tg_bancodedados`.`aut_autorizacao`
+  (`aut_nome`)
 VALUES
   ('ROLE_SUP');
 
---rollback delete from tg_bancodedados.AUT_AUTORIZACAO where AUT_NOME='ROLE_SUP';
+--rollback delete from tg_bancodedados.aut_autorizacao where AUT_NOME='ROLE_SUP';
 
 --changeset andre:13
-INSERT INTO tg_bancodedados.AUT_AUTORIZACAO
-  (AUT_NOME)
+INSERT INTO `tg_bancodedados`.`aut_autorizacao`
+  (`aut_nome`)
 VALUES
   ('ROLE_ADMIN');
 
---rollback delete from tg_bancodedados.AUT_AUTORIZACAO where AUT_NOME='ROLE_ADMIN';
+--rollback delete from tg_bancodedados.aut_autorizacao where AUT_NOME='ROLE_ADMIN';
 
 --changeset andre:14
-INSERT INTO tg_bancodedados.UAU_USUARIO_AUTORIZACAO
-  (usu_id, AUT_ID)
+INSERT INTO `tg_bancodedados`.`uau_usuario_autorizacao`
+  (usu_id, aut_id)
 SELECT usu_id, AUT_ID
-FROM usuario, AUT_AUTORIZACAO
+FROM usuario, aut_autorizacao
 WHERE usu_nome = 'Gustavo'
   AND AUT_NOME = 'ROLE_DESEN';
 
 --changeset andre:15
-INSERT INTO tg_bancodedados.UAU_USUARIO_AUTORIZACAO
+INSERT INTO tg_bancodedados.uau_usuario_autorizacao
   (usu_id, AUT_ID)
 SELECT usu_id, AUT_ID
-FROM usuario, AUT_AUTORIZACAO
+FROM usuario, aut_autorizacao
 WHERE usu_nome = 'Murilo'
   and aut_nome ="ROLE_ADMIN"
 ;
 
 --changeset andre:16
-INSERT INTO tg_bancodedados.UAU_USUARIO_AUTORIZACAO
+INSERT INTO tg_bancodedados.uau_usuario_autorizacao
   (usu_id, AUT_ID)
 SELECT usu_id, AUT_ID
-FROM usuario, AUT_AUTORIZACAO
+FROM usuario, aut_autorizacao
 WHERE usu_nome = 'Ronaldo'
   and aut_nome ="ROLE_SUP" ;
--- target/classes/db/changelog/scripts/script_maquete.sql
+++ target/classes/db/changelog/scripts/script_maquete.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql 
 
 --changeset andre:17
-INSERT INTO tg_bancodedados.MAQUETE
+INSERT INTO `tg_bancodedados`.`maquete`
     (maqu_id, maqu_descricao,maqu_data,maqu_local,maqu_projeto,maqu_cliente)
 VALUES(202019, 'PROJETO DE GRADUACAO', '2015-09-22', 'SJC/SP', 'REFINARIA', 'FATEC');
 
