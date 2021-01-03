17ea99daf119ed2a2c84a2c18e91f1f90f2f9fd2
AndreMaurilio <andre.m.s.castro@outlook.com>
2021-01-03 20:30:12 -0300

    alterado nome da maquete no script do liquibase

--- src/main/resources/db/changelog/scripts/script_maquete.sql
+++ src/main/resources/db/changelog/scripts/script_maquete.sql
@@ -3,6 +3,6 @@
 --changeset andre:17
 INSERT INTO `tg_bancodedados`.`maquete`
     (maqu_id, maqu_descricao,maqu_data,maqu_local,maqu_projeto,maqu_cliente)
-VALUES(202019, 'PROJETO DE GRADUACAO', '2015-09-22', 'SJC/SP', 'REFINARIA', 'FATEC');
+VALUES(202019, 'REFINARIA DE SÃO PAULO', '2015-09-22', 'SJC/SP', 'REFINARIA', 'FATEC');
 
 --rollback delete from tg_bancodedados.MAQUETE where maqu_id = 202019;
